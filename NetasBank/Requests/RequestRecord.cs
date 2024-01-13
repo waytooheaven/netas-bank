5e09a43c11c0f633eeaaeb9f5fd1f47612e4fa38
waytooheaven <anilcodingninja@yahoo.com>
2024-01-13 18:04:32 +0300

    abstract record

--- NetasBank/Requests/RequestRecord.cs
+++ NetasBank/Requests/RequestRecord.cs
@@ -4,7 +4,7 @@ using NetasBank.Enums;
 namespace NetasBank.Requests;
 
 [Bind("TransactionId,Amount,BankId")]
-public record RequestRecord
+public abstract record RequestRecord
 {
     public Guid TransactionId { get; set; }
     public int Amount { get; set; }
