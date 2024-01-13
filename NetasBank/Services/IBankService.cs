6b8e6ea78bcb369296bfa4a5c9be885705fbb5d4
waytooheaven <anilcodingninja@yahoo.com>
2024-01-13 18:05:07 +0300

    format

--- NetasBank/Services/IBankService.cs
+++ NetasBank/Services/IBankService.cs
@@ -7,6 +7,5 @@ public interface IBankService
     Task<bool> Pay(CreateTransactionDetailsRequestRecord request);
     Task<bool> Cancel(CancelTransactionRequestRecord request);
     Task<bool> Refund(RefundTransactionRequestRecord request);
-
     Task<ICollection<ReportingResponse>> Report(ReportingRequest request);
 }
